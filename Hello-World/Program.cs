41411aae5c03d7838baa1dafa7a786a043890f76
Julius Sonko <jsonko@my.edgetechacademy.edu>
2020-04-28 08:09:01 -0500

    Add project files.

---
+++ Hello-World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Matrix Has You...");
+            Console.ReadLine();
+        }
+    }
+}
